a1a507bd0a2f22bc445162484a9ad7640cd6d809
camone <106678583+camoneart@users.noreply.github.com>
2025-12-30 01:00:17 +0900

    chore: about page style

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -18,18 +18,20 @@ const AboutPage = () => {
 									<h2 className={styles["about-section-title"]}>はじめに</h2>
 								</div>
 							</div>
-							<p className={styles["about-section-message"]}>
-								「<em>OUTPUT QUEST　叡智の継承者</em>」は、アウトプットで知識を磨き、勇者を育てる
-								"RPG風学習支援Webアプリ" です。
-							</p>
-							<p className={styles["about-section-message"]}>
-								この世界では <em>Zenn</em>
-								で記事を書くことで経験値を獲得することができ、勇者のレベルを上げることができます。
-							</p>
-							<p className={styles["about-section-message"]}>
-								アウトプットを続けることで、あなたは<em>「叡智の継承者」</em>
-								として、優れた知識と知恵を未来へとつないでいくのです。
-							</p>
+							<div>
+								<p className={styles["about-section-message"]}>
+									「<em>OUTPUT QUEST　叡智の継承者</em>」は、アウトプットで知識を磨き、勇者を育てる
+									"RPG風学習支援Webアプリ" です。
+								</p>
+								<p className={styles["about-section-message"]}>
+									この世界では <em>Zenn</em>
+									で記事を書くことで経験値を獲得することができ、勇者のレベルを上げることができます。
+								</p>
+								<p className={styles["about-section-message"]}>
+									アウトプットを続けることで、あなたは<em>「叡智の継承者」</em>
+									として、優れた知識と知恵を未来へとつないでいくのです。
+								</p>
+							</div>
 						</section>
 
 						<section className={`${styles["about-section"]}`}>
