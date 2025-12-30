b6c7bda59e0a1494f4164efa38305aebee914b72
camone <106678583+camoneart@users.noreply.github.com>
2025-12-30 18:53:01 +0900

    add: 宝箱のアイコンを追加

--- src/features/items/components/items-treasure-chest-icon/ItemsTreasureChestIcon.tsx
+++ src/features/items/components/items-treasure-chest-icon/ItemsTreasureChestIcon.tsx
@@ -18,14 +18,14 @@ const ItemsTreasureChestIcon: React.FC<ItemsTreasureChestProps> = ({
 	return (
 		<div className={className}>
 			<Image
-				src="/images/items-page/unacquired-icon/treasure-chest-close01.png"
+				src="/images/items-page/unacquired-icon/treasure-chest-close-icon01.png"
 				alt="未入手のアイテム"
 				width={width}
 				height={height}
 				className={classNameClose}
 			/>
 			<Image
-				src="/images/items-page/unacquired-icon/treasure-chest-open01.png"
+				src="/images/items-page/unacquired-icon/treasure-chest-open-icon01.png"
 				alt="未入手のアイテム"
 				width={width}
 				height={height}
