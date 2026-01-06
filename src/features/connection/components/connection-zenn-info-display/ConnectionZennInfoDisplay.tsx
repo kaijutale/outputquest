45f55d8b421406a02587875002ab24f4cc3c8d1b
camone <106678583+camoneart@users.noreply.github.com>
2026-01-07 03:10:09 +0900

    style(connection): adjust Zenn info display layout

--- src/features/connection/components/connection-zenn-info-display/ConnectionZennInfoDisplay.tsx
+++ src/features/connection/components/connection-zenn-info-display/ConnectionZennInfoDisplay.tsx
@@ -22,26 +22,21 @@ const ConnectionZennInfoDisplay: React.FC<ConnectionZennInfoDisplayProps> = ({
 	return (
 		<div className={styles["zenn-info-container"]}>
 			<div className={styles["zenn-info-content"]}>
-				<h2 className={styles["zenn-info-title"]}>
-					<Image
-						src="/images/connection/connection-zenn-logo.svg"
-						alt="Zenn"
-						width={16}
-						height={16}
-						className={styles["zenn-logo-sm"]}
-					/>
-					<span>Zenn連携情報</span>
-				</h2>
+				<div className={styles["zenn-info-title-box"]}>
+					<h2 className={styles["zenn-info-title"]}>
+						<Image
+							src="/images/connection/connection-zenn-logo.svg"
+							alt="Zenn"
+							width={16}
+							height={16}
+							className={styles["zenn-logo-sm"]}
+						/>
+						<span>Zenn連携情報</span>
+					</h2>
+				</div>
 				<div className="grid gap-6 md:gap-3">
 					<dl className={styles["user-info-list"]}>
 						<dt className={styles["user-info-title"]}>
-							<Image
-								src="/images/connection/user-icon.svg"
-								alt="ユーザーアイコン"
-								width={17}
-								height={17}
-								className={styles["user-info-icon"]}
-							/>
 							<span className={styles["user-info-title-text"]}>ユーザー名：</span>
 						</dt>
 						<dd className={styles["user-info-description"]}>@{userInfo.zennUsername}</dd>
@@ -49,13 +44,6 @@ const ConnectionZennInfoDisplay: React.FC<ConnectionZennInfoDisplayProps> = ({
 
 					<dl className={styles["article-count-list"]}>
 						<dt className={styles["article-count-title"]}>
-							<Image
-								src="/images/connection/article.svg"
-								alt="記事のアイコン"
-								width={17}
-								height={17}
-								className={styles["article-count-icon"]}
-							/>
 							<span className={styles["article-count-title-text"]}>投稿した記事：</span>
 						</dt>
 						<dd className={styles["article-count-description"]}>
