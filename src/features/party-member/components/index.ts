4ea9e1aa53f1d84ffb46514c90eb0daec0e7f200
camone <106678583+camoneart@users.noreply.github.com>
2026-01-06 19:16:14 +0900

    remove: party-member-rarity-star

--- src/features/party-member/components/index.ts
+++ src/features/party-member/components/index.ts
@@ -1,13 +1,6 @@
-import PartyMemberRarityStar from "./party-member-rarity-star/PartyMemberRarityStar";
 import PartyMemberFooter from "./party-member-footer/PartyMemberFooter";
 import PartyMemberDetail from "./party-member-detail/PartyMemberDetail";
 import PartyMemberDynamicHead from "./party-member-dynamic-head/PartyMemberDynamicHead";
 import PartyMemberDetailSkeleton from "./party-member-detail-skeleton/PartyMemberDetailSkeleton";
 
-export {
-	PartyMemberRarityStar,
-	PartyMemberFooter,
-	PartyMemberDetail,
-	PartyMemberDynamicHead,
-	PartyMemberDetailSkeleton,
-};
+export { PartyMemberFooter, PartyMemberDetail, PartyMemberDynamicHead, PartyMemberDetailSkeleton };
