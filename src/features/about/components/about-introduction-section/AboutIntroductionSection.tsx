def13c6f0dbfde55d3901658579a8d9a2edcd0c2
camone <106678583+camoneart@users.noreply.github.com>
2026-01-09 03:01:50 +0900

    fix: update About page content to match current specs and world view

--- src/features/about/components/about-introduction-section/AboutIntroductionSection.tsx
+++ src/features/about/components/about-introduction-section/AboutIntroductionSection.tsx
@@ -11,10 +11,11 @@ export default function AboutIntroductionSection() {
 					「<em>OUTPUT QUEST　叡智の継承者</em>」は、アウトプットで知識を磨き、勇者を育てる
 					&quot;RPG風学習支援Webアプリ&quot; です。
 				</p>
-				<p className={styles["about-section-message"]}>
-					この世界では <em>Zenn</em>
-					で記事を書くことで経験値を獲得することができ、勇者のレベルを上げることができます。
-				</p>
+				<div className={styles["about-section-message"]}>
+					この世界では、
+					<em>あなたの現実世界でのアウトプット（Zennへの投稿）が、そのまま勇者の力となります。</em>
+					<p>記事を書くことで経験値を獲得し、勇者を成長させましょう。</p>
+				</div>
 				<p className={styles["about-section-message"]}>
 					アウトプットを続けることで、あなたは<em>「叡智の継承者」</em>
 					として、優れた知識と知恵を未来へとつないでいくのです。
