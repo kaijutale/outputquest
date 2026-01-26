dde3a07ca2b66b6904178a3a814e3bc976041027
camone <106678583+camoneart@users.noreply.github.com>
2026-01-26 20:18:39 +0900

    chore: 重複コメントを削除

--- src/features/home/data/homeCharacterDisplayOrder.ts
+++ src/features/home/data/homeCharacterDisplayOrder.ts
@@ -1,6 +1,3 @@
 // 表示順序を管理する配列（homeCharacterImagesのidを参照）
 // キャラクターの配置順（画面上の位置）
 export const homeCharacterDisplayOrder: number[] = [5, 12, 10, 7, 4, 1, 2, 3, 9, 8, 11, 6];
-
-// キャラの配置順
-// 5, 12, 10, 7, 4, 1, 2, 3, 9, 8, 11, 6,
